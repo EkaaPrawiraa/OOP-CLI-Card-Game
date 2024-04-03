ad8c34b6051b139395155e97d86490167fcaf0b1
ZachS17 <130656022+ZachS17@users.noreply.github.com>
2024-04-03 23:31:44 +0700

    add: miscconfig class
    4 sekawan dan getter

--- src/Configuration/MiscConfig.hpp
+++ src/Configuration/MiscConfig.hpp
@@ -3,7 +3,7 @@
 
 class MiscConfig
 {
-public:
+private:
     int winningMoney;
     int winningWeight;
     int storageRows;
@@ -13,10 +13,22 @@ public:
     int farmRows;
     int farmCols;
 
+public:
+    MiscConfig();
     MiscConfig(int winningMoney, int winningWeight, int storageRows, int storageCols, int landRows, int landCols, int farmRows, int farmCols);
+    ~MiscConfig();
+    MiscConfig &operator=(const MiscConfig &);
     void display() const;
+    int getWinningMoney() const;
+    int getWinningWeight() const;
+    int getStorageRows() const;
+    int getStorageCols() const;
+    int getLandRows() const;
+    int getLandCols() const;
+    int getFarmRows() const;
+    int getFarmCols() const;
 };
 
-#endif 
+#endif
 
 // NOTES INI HANYA KELAS COBA COBA UNTUK TEST WORDMACHINE
--- src/MiscConfig.cpp
+++ src/MiscConfig.cpp
@@ -1,8 +1,30 @@
 #include "Configuration\MiscConfig.hpp"
 #include <iostream>
 
+MiscConfig::MiscConfig() : winningMoney(0), winningWeight(0), storageRows(0), storageCols(0), landRows(0), landCols(0), farmRows(0), farmCols(0) {}
+
 MiscConfig::MiscConfig(int winningMoney, int winningWeight, int storageRows, int storageCols, int landRows, int landCols, int farmRows, int farmCols)
-    : winningMoney(winningMoney), winningWeight(winningWeight), storageRows(storageRows), storageCols(storageCols), landRows(landRows), landCols(landCols), farmRows(farmRows), farmCols(farmCols) {}
+    : winningMoney(winningMoney), winningWeight(winningWeight), storageRows(storageRows), storageCols(storageCols), landRows(landRows), landCols(landCols), farmRows(farmRows), farmCols(farmCols)
+{
+}
+
+MiscConfig::~MiscConfig()
+{
+    std::cout << "MiscConfig dihapus" << std::endl;
+}
+
+MiscConfig &MiscConfig::operator=(const MiscConfig &original)
+{
+    winningMoney = original.winningMoney;
+    winningWeight = original.winningWeight;
+    storageRows = original.storageRows;
+    storageCols = original.storageCols;
+    landRows = original.landRows;
+    landCols = original.landCols;
+    farmRows = original.farmRows;
+    farmCols = original.farmCols;
+    return *this;
+}
 
 void MiscConfig::display() const
 {
@@ -11,4 +33,44 @@ void MiscConfig::display() const
     std::cout << "Storage Size: " << storageRows << " x " << storageCols << std::endl;
     std::cout << "Land Size: " << landRows << " x " << landCols << std::endl;
     std::cout << "Farm Size: " << farmRows << " x " << farmCols << std::endl;
+}
+
+int MiscConfig::getWinningMoney() const
+{
+    return winningMoney;
+}
+
+int MiscConfig::getWinningWeight() const
+{
+    return winningWeight;
+}
+
+int MiscConfig::getStorageRows() const
+{
+    return storageRows;
+}
+
+int MiscConfig::getStorageCols() const
+{
+    return storageCols;
+}
+
+int MiscConfig::getLandRows() const
+{
+    return landRows;
+}
+
+int MiscConfig::getLandCols() const
+{
+    return landCols;
+}
+
+int MiscConfig::getFarmRows() const
+{
+    return farmRows;
+}
+
+int MiscConfig::getFarmCols() const
+{
+    return farmCols;
 }
