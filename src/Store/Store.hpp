3e80e9a5501bc141aa06733c26f359cd8a587348
OrdinaryAzazel <19622052@mahasiswa.itb.ac.id>
2024-04-06 23:59:00 +0700

    update

---
+++ src/Store/Store.hpp
@@ -0,0 +1,21 @@
+#include "..\\PlantAnimal\\Plant.hpp"
+#include "..\\PlantAnimal\\Animal.hpp"
+#include "..\\Product\\Product.hpp"
+#include "..\\Building\\Building.hpp"
+#include <vector>
+
+using namespace std;
+
+class Store{
+    private:
+    vector<Building> buildings;
+    vector<Plant> plants;
+    vector<Animal> animals;
+    vector<Product> products;
+
+
+    public:
+
+
+
+};
